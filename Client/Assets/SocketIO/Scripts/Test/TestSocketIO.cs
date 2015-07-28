405fdcae6ee7da795ed15570bc82f727fa33155b
teyou <rudy@Rudy.local>
2015-07-28 15:59:47 +0800

    + gitignore for Unity3d project - add Client side Unity3d code

---
+++ Client/Assets/SocketIO/Scripts/Test/TestSocketIO.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System;
+
+using System.Collections;
+using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
+
+public class TestSocketIO : MonoBehaviour
+{
+	private SocketIOComponent socket;
+	private Color _bgColor = Color.black;
+
+	public void Start() 
+	{
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+		socket.On ("rgb",changeBackgroundRGB);
+	}
+
+	public void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			_bgColor = Color.green;
+		}
+
+		Camera.main.backgroundColor = _bgColor;
+	}
+	
+	public void changeBackgroundRGB(SocketIOEvent e)
+	{
+		Debug.Log("[SocketIO] changeBackgroundRGB received: " + e.name + " " + e.data);
+		JSONObject jo = e.data as JSONObject;
+
+		Color bgColor = new Color (
+								Convert.ToSingle(jo["r"].str) / 255f,
+								Convert.ToSingle(jo["g"].str) / 255f, 
+								Convert.ToSingle(jo["b"].str) / 255f,
+								1f);
+		_bgColor = bgColor;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Unity_POC_Socket
 POC using web Socket to interact with Unity APP
+
+
+/Server/
+
+- index.js : Server side node express code
+- /public/index.html : The color controller using Socket.IO 
+
+
+
+/Client/
+
+This contains Unity3D code for showing the result (aka color), the color is can change in realtime via index.html
+
